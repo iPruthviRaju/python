8eea6cf4168d5a9b145d60664800350e0aed9d0f
Pruthvi Raju <pruthvi.raju@live.com>
2018-07-22 23:56:19 +0530

    Math things

---
+++ 0002_math/math.py
@@ -0,0 +1,11 @@
+print("PEMDAS")
+print("Paranthesis, Exponential, Multiplication, Divide, Addition, Substraction.")
+
+x = 5 * 10
+
+if x == 50:
+    print("true - x is " + str(x))
+else:
+    print("false - x is " +  str(x))
+    
+print(str(10 / 5 * 1))
